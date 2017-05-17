b1030b670114def30ab33dedc4c5cef6cb1fd8b1
Andrew Nyaisonga <anyaison@u.rochester.edu>
2017-05-17 17:35:06 -0400

    Add files via upload

---
+++ Intersection.java
@@ -0,0 +1,13 @@
+/**
+ * Created by andrewnyaisonga on 4/29/17.
+ */
+public class Intersection {
+
+    String IntersectionID;
+    double distance;
+    double longitude;
+    double latitude;
+    boolean known;
+    Intersection path;
+
+}
