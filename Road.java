64ec4f8f51a0ae62f0aedb4e7553d455f9c6a928
Andrew Nyaisonga <anyaison@u.rochester.edu>
2017-05-17 17:34:40 -0400

    Add files via upload

---
+++ Road.java
@@ -0,0 +1,18 @@
+/**
+ * Created by andrewnyaisonga on 4/29/17.
+ */
+public class Road {
+
+    String roadID;
+    String intersect1;
+    String intersect2;
+    double distance;
+
+    public Road(String road, String int1, String int2, double dist) {
+        roadID = road;
+        intersect1 = int1;
+        intersect2 = int2;
+        distance = dist;
+    }
+
+}
